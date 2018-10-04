119705a18eb15a42339dd52cc6c53ea47bd34a73
Sidharth Das <sid@Sidharths-MacBook-Pro.local>
2018-10-04 09:00:21 +0100

    Fixed back button and JSON rendering issues

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from funcs.fuzzy_matching import name_similarity_array
 
 def validate_payment(name, sort_code, account_number, pass_threshold):
 
+    #Format - Name, sort_code, account_number, match_percentage, outcome, error_message 
     output_array = [name, sort_code, account_number, '-', '-', '-']
     
     if (valid_sort_code(sort_code) == False or valid_account_number(account_number) == False):
         
-        output_array[4] = 'Invalid account number or sort code'
+        output_array[5] = 'Invalid account number or sort code'
 
     else:
         result_name = fetch_name_sql(account_number, sort_code)
@@ -31,7 +32,6 @@ def validate_payment(name, sort_code, account_number, pass_threshold):
             output_array[4] = 'NA'
             output_array[5] = 'Reference payee name not found'
     
-            
-    output_json = "{'name': '" + str(output_array[0]) + "', 'sort_code': '" + str(output_array[1]) + "', 'account_number': '" + str(output_array[2]) + "', 'match_percentage': '" + str(output_array[3]) + "', 'payment_status': '" + str(output_array[4]) + "', 'error_message': '" + str(output_array[5]) + "'}"
+    output_tuples_array = [('name', str(output_array[0])), ('sort_code', str(output_array[1])), ('account_number', str(output_array[2])), ('match_percentage', str(output_array[3])), ('payment_status', str(output_array[4])), ('error_message', str(output_array[5]))]
     
-    return (json.dumps(output_json))
+    return (json.dumps(dict(output_tuples_array)))
